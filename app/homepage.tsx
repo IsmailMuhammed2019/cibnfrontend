4700396989e22b6b1001ff62e8801b202743ffd2
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2024-12-13 11:43:46 +0100

    update initial codes

---
+++ app/homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function homepage() {
+  return (
+    <div>
+      <Header />
+      All content are been upload...page coming up soon...
+      <Footer />
+    </div>
+  )
+}
+
+export default homepage
