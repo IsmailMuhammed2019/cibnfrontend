4bdc3c89b7b06de5acf907e8c2691702b335011f
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2024-12-13 13:34:37 +0100

    updata paage codes

--- app/homepage.tsx
+++ app/homepage.tsx
@@ -8,13 +8,11 @@ import './homepage.css'
 function homepage() {
   return (
     <div className="coming-soon-container">
-      <Header />
       <div className="coming-soon">
         <FontAwesomeIcon icon={faHourglassHalf} className="spinning-icon" size="3x" />
         <h2>Coming Soon</h2>
         <p>We are working hard to bring you something amazing. Stay tuned!</p>
       </div>
-      <Footer />
     </div>
   )
 }
