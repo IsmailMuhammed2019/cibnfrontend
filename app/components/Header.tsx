6a85d53b9b3fd0b7d21aafd83177d73d2d7e7449
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2024-12-16 13:28:56 +0100

    update responsive header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,24 +1,30 @@
 "use client"; // Add this directive at the top
 
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'; // Import the CSS file
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage menu visibility
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header>
       <div className="header-container">
         <div className="header-info">
-          <img src="/imgs/phone.svg" alt="Phone" style={{ marginRight: '10px' }} className="iconssizes" />
-          <span>0700-DIAL-CIBN</span>
-          <img src="/imgs/email.svg" alt="Email" style={{ marginLeft: '20px', marginRight: '10px'}} className="iconssizes" />
+          <img src="/imgs/phone.svg" alt="Phone" className="iconssizes" />
+          <span className="contact-info">0700-DIAL-CIBN</span>
+          <img src="/imgs/email.svg" alt="Email" className="iconssizes" />
           <span>cibn@cibn.org</span>
         </div>
         <div className="header-info">
           <span>Connect with us:</span>
-          <img src="/imgs/facebook.svg" alt="Facebook" className="iconssizes" style={{ marginLeft: '10px', marginRight: '5px' }} />
-          <img src="/imgs/twitter.svg" alt="Twitter" className="iconssizes" style={{ marginLeft: '5px', marginRight: '5px' }} />
-          <img src="/imgs/linkedln.svg" alt="LinkedIn" className="iconssizes" style={{ marginLeft: '5px', marginRight: '5px' }} />
-          <img src="/imgs/youtube.svg" alt="YouTube" style={{ marginLeft: '5px' }} />
+          <img src="/imgs/facebook.svg" alt="Facebook" className="iconssizes" />
+          <img src="/imgs/twitter.svg" alt="Twitter" className="iconssizes" />
+          <img src="/imgs/linkedln.svg" alt="LinkedIn" className="iconssizes" />
+          <img src="/imgs/youtube.svg" alt="YouTube" className="iconssizes" />
           <button className="header-button">Login</button>
         </div>
       </div>
@@ -28,23 +34,22 @@ function Header() {
         <div className="logo">
           <img src="/imgs/logo.png" alt="Logo" className="logo-image" />
           <div className="logo-text">
-            <span>The Chartered Institute</span><br/>
+            <span>The Chartered Institute</span><br />
             <span>Of Bankers Of Nigeria</span>
           </div>
         </div>
-        <nav className="nav-menu">
+        <button className="hamburger" onClick={toggleMenu}>
+          &#9776; {/* Hamburger icon */}
+        </button>
+        <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
           <ul>
             {['The Institute', 'Membership', 'Capacity Building & Certification', 'Learning', 'Resource Hub', 'Publication'].map((item) => (
               <li key={item}>
                 {item} <img src="/imgs/dropdown.svg" alt="Dropdown" className="menu-icon" />
               </li>
             ))}
-            <li>
-              Events 
-            </li>
-            <li>
-              Job Portal
-            </li>
+            <li>Events</li>
+            <li>Job Portal</li>
           </ul>
         </nav>
         <button className="join-button">Join CIBN</button>
