719db5d3150aa5d38a4b2e080c50bfb472b920d3
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2024-12-17 14:15:27 +0100

    update

--- app/homepage.tsx
+++ app/homepage.tsx
@@ -26,6 +26,28 @@ function Homepage() {
           </div>
         </div>
       </div>
+      
+      <div className='bigImage'>
+        <div className='prof'>
+          <img src="/imgs/prof.jpg" alt="Profile" style={{ width: '100%', height: 'auto', objectFit: 'cover', borderRadius: '20px' }} />
+        </div>
+        <div className='others'>
+          <div className='div1'>
+            <h1 className='title1'>Message from the President/
+            Chairman of the Council</h1>
+            <h2 className='subtitle'>Professor Pius Deji Olanrewaju, Ph.D, FCIB</h2>
+            <p className='pagecode'>You are welcome to the official website of The Chartered Institute of Bankers of Nigeria (CIBN)...</p>
+
+            <button className='readmore'>Read More ></button>
+          </div>
+          <div className='div2'>
+          <h1 className='title1'>2024 Calendar of Activities</h1>
+            <h2 className='subtitle'>Our program and training courses are fashioned to sharpen your skills and broaden your knowledge of the Nigerian banking practices.</h2>
+
+            <button className='readmore'>Read More ></button>
+          </div>
+        </div>
+      </div>
 
       <Footer />
     </div>
