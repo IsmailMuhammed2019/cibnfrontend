95dd31c132ca8101045dfc7f59436937cb290206
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2024-12-18 09:43:17 +0100

    update logos name

--- app/components/sponsors.tsx
+++ app/components/sponsors.tsx
@@ -17,18 +17,21 @@ const Sponsors = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const logosToShow = 4; // Number of logos to show at a time
 
+    // Function to move to the next set of logos
     const nextLogos = () => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % (images.length - logosToShow + 1));
     };
 
-    const prevLogos = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + (images.length - logosToShow + 1)) % (images.length - logosToShow + 1));
-    };
+    // Automatically move logos every 3 seconds
+    useEffect(() => {
+        const interval = setInterval(nextLogos, 3000); // Change logo every 3 seconds
+        return () => clearInterval(interval); // Clear interval on component unmount
+    }, []);
 
     return (
         <div className="sponsors-container">
             <div className="sponsors-controls">
-                <button className="sponsor-control left" onClick={prevLogos}>&#10094;</button>
+                <button className="sponsor-control left" onClick={() => setCurrentIndex((prevIndex) => (prevIndex - 1 + (images.length - logosToShow + 1)) % (images.length - logosToShow + 1))}>&#10094;</button>
                 <div className="sponsors-logos">
                     {images.slice(currentIndex, currentIndex + logosToShow).map((logo, index) => (
                         <img key={index} src={`/imgs/${logo}`} alt={`Sponsor ${index + 1}`} className="sponsor-logo" />
