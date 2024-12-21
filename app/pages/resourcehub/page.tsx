5c27bed602c0d4cb080b179416c7b74ddae47599
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2024-12-21 23:36:28 +0100

    update resource hub

--- app/pages/resourcehub/page.tsx
+++ app/pages/resourcehub/page.tsx
@@ -2,11 +2,31 @@
 import React from 'react';
 import Layout from '../../layout/Layout'; // Adjust the path as necessary
 import Sponsors from '@/app/components/sponsors';
+import "./resource.css"
 const ResourceHub = () => {
     return (
         <Layout>
-            <h1>Coming Soon</h1>
-            <p>Something is coming soon</p>
+            <div className="theinstitute">
+        <div className="theinstitute-main">
+          <div className="theinstitute-content">
+            <h1 className="theinstitutetitle">CIBN - Resource Hub</h1>
+            <p>
+            At the Chartered Institute of Bankers of Nigeria, we make it a duty to <br />keep all members afloat of every activity to ensure they are well <br />updated and prepared.
+            </p>
+            <button>
+              Read More{" "}
+              <img
+                src="/imgs/down1.svg"
+                alt="Dropdown"
+                className="dropdown-icon"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        
+      </div>
             <Sponsors />
         </Layout>
     );
